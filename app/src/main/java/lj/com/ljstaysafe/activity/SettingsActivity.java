5c30898782a3f3eacba77e78f61f22c956117ede
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2018-08-19 20:29:06 +0700

    - added Settings activity - implemented click on Settings in Me Fragment goes to Settings Activity - implemented clicking toolbar's back button goes back to Me Fragment

---
+++ app/src/main/java/lj/com/ljstaysafe/activity/SettingsActivity.java
@@ -0,0 +1,29 @@
+package lj.com.ljstaysafe.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import lj.com.ljstaysafe.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
--- app/src/main/java/lj/com/ljstaysafe/fragment/MeFragment.java
+++ app/src/main/java/lj/com/ljstaysafe/fragment/MeFragment.java
@@ -1,16 +1,21 @@
 package lj.com.ljstaysafe.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import lj.com.ljstaysafe.R;
+import lj.com.ljstaysafe.activity.SettingsActivity;
 
-public class MeFragment extends Fragment {
+public class MeFragment extends Fragment implements ConstraintLayout.OnClickListener {
+
+    private ConstraintLayout layoutSettings;
 
 
     public MeFragment() {
@@ -25,7 +30,19 @@ public class MeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        View view =  inflater.inflate(R.layout.fragment_me, container, false);
+        layoutSettings = view.findViewById(R.id.layoutSettings);
+        layoutSettings.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.layoutSettings:
+                Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
