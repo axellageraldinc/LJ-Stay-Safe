6b3169d77ba3583832f2e4eed5b750721bf13382
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2018-08-20 11:03:14 +0700

    - added See Driving Score Activity

---
+++ app/src/main/java/lj/com/ljstaysafe/activity/SeeDrivingScoreActivity.java
@@ -0,0 +1,15 @@
+package lj.com.ljstaysafe.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lj.com.ljstaysafe.R;
+
+public class SeeDrivingScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_driving_score);
+    }
+}
